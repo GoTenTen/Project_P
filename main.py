c56c189a3ad17eb93dfae268d01c893496f49d87
LASCII <kevindo.perso@gmail.com>
2025-10-04 02:16:41 +0200

    j'aoute le main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+def game_start():
+
+    print("Jouer?")
+    start = input("Yes or No")
+
+    if start == "yes":
+        print("go")
+    else:
+        print('nique ta grand mere')
+
+
+game_start()
