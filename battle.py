5a63b0b338f3b5fd5d80837ab792abc0e3f84510
LASCII <kevindo.perso@gmail.com>
2025-10-04 03:19:20 +0200

    ajout du fichier class.py et modif de main.py et battle.py

--- battle.py
+++ battle.py
@@ -1,6 +1,13 @@
+import time
+import random
 
 def start():
-    print('test')
-    print('test')
+    player1 = str(input('Veuillez définir le nom du Joueur1 s\'il vous plait.   '))
+    player2 = str(input('Veuillez définir le nom du Joueur2 s\'il vous plait.   '))
+    print('')
+
+    print(player1, player2)
+
+
 
 
---
+++ class.py
@@ -0,0 +1,9 @@
+
+class Pou:
+    def __init__(self, name, hp, atk):
+        self.name = name
+        self.hp = hp
+        self.atk = atk
+
+Pou1 = Pou("Pou", 35, 5)
+Pou2 = Pou("Pou", 35, 5)
