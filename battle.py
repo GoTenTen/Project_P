d2dccfe55b9f61ebca2334ec87e5923d3d8b69ad
LaMenace <butetjason26@gmail.com>
2025-10-04 05:29:19 +0200

    add class comps et pou + fonction de base (is_alive, take_damage etc...)

--- battle.py
+++ battle.py
@@ -1,6 +1,7 @@
 import time
 import random
-
+from class_poukemon import *
+'''
 def start():
     player1 = str(input('Veuillez définir le nom du Joueur1 s\'il vous plait.   '))
     player2 = str(input('Veuillez définir le nom du Joueur2 s\'il vous plait.   '))
@@ -8,6 +9,51 @@ def start():
 
     print(player1, player2)
 
+team1 = {'Pou1' : P.Pou1}
+team2 = {'Pou2' : P.Pou2}
+
+teams = [team1, team2]'''
+
+def is_alive(self):
+    return self.hp > 0 
+
+def t_damage(self, x): #take_damage
+    self.hp -= x  
+    if self.hp < 0:
+        self.hp = 0
+
+def use_c(self, Pou): #use_comps
+    if self.sign == "*":
+        return Pou.atk * self.mult
+    if self.sign == "+":
+        return Pou.atk + self.mult
+    #Prend en entree une competence et verifie le signe afin de faire le calcul adéquat
+
+def malus(self, P):
+    if self.sign == "-":    
+        return P.atk - self.mult
+    if self.sign == "/":    
+        return P.atk / self.mult
+    #Meme principe que use_c mais pour les debuff
+
+def apply_c(self, user, target = None):
+    if self.sign == "*":
+        return user.atk * self.mult
+    elif self.sign == "+":
+        return user.atk + self.mult
+    elif self.sign == "-":
+        if target:
+            return target.atk - self.mult
+    elif self.sign == "/":    
+        if target:
+            return target.atk / self.mult
+    
 
 
+def test():
+    print(f"Les hp de Pou1 sont {Pou1.hp}")
+    t_damage(Pou1, malus(chidori3, Pou2))
+    print(f"Et maintenant {Pou1.hp}")
+    is_alive(Pou1)
 
+test()
---
+++ class_poukemon.py
@@ -0,0 +1,65 @@
+
+class Pou:
+    def __init__(self, name, hp, max_hp, atk, nickname = None):
+        self.name = name
+        self.nickname = nickname
+        self.hp = hp
+        self.max_hp = hp
+        self.atk = atk
+
+    def is_alive(self):
+        return self.hp > 0
+    
+    def t_damage(self, x): #take_damage
+        self.hp -= x  
+        if self.hp < 0:
+            self.hp = 0
+
+P = Pou #alias permettant d'appeler la classe p au lieu de pou
+
+class comps:
+    def __init__(self, name, mult, sign, type):
+        self.name = name
+        self.mult = mult
+        self.sign = sign # '*', '+', '-', '/'
+        self.type = type #buff/debuff/attaque/heal
+
+    def use_c(self, user, target = None):
+        if self.type != "heal":
+            if self.sign == "*":
+                return user.atk * self.mult
+            elif self.sign == "+":
+                return user.atk + self.mult
+            elif self.sign == "-":
+                if target:
+                    target.atk = target.atk - self.mult
+                    return target.atk
+            elif self.sign == "/":    
+                if target:
+                    target.atk = target.atk / self.mult
+                    return target.atk
+        else:
+            if self.hp < self.max_hp:
+                return self.hp + (self.max_hp / 3)
+
+
+Pou1 = P("Pou", 35, 5)
+Pou2 = P("Pou", 35, 5)
+
+chidori = comps("chi", 2, "*", 'buff')
+chidori2 = comps("chi2", 2, "+",'buff')
+chidori3 = comps("chi3", 2, "/",'debuff')
+'''chidori4 = comps("chi4", None, "+", 'heal')'''
+
+
+def test():
+    print(f"Les hp de Pou1 sont {Pou1.hp}\n")
+    print(f"Les hp de Pou2 sont {Pou2.hp}\n")
+    chidori3.use_c(Pou1, Pou2)
+    print(f"L'attaque de pou2 baisse de 5 a {Pou2.atk}\n")
+    Pou1.t_damage(Pou2.atk)
+    print(f"Les hp de Pou1 sont {Pou1.hp}\n")
+    '''chidori4.use_c(Pou1, Pou1)
+    print(f"Les hp de Pou1 sont {Pou1.hp}\n")'''
+    
+test()
