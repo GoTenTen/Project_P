22da54384f56a833189fbe7faa09dbda7aba39e0
LASCII <kevindo.perso@gmail.com>
2025-10-06 00:04:38 +0200

    Création du fichier 'class_poukemon_list' pour faciliter la création de nouveaux poux.
    - Changement de nom pour 'Pou' -> 'Pou Soldat', 'Gros Pou' -> 'Pou Sergent' et 'Pipou Pou' -> 'Pou Kamikaze'
    - Ajout de 'Pou Accro au Casino' et 'Pou BDSM' (pas encore utilisable sans choix de team)

---
+++ class_poukemon_list.py
@@ -0,0 +1,37 @@
+#class_poukemon_list
+from skills_list import *
+
+PouModels = {
+    "Pou Soldat": {
+        "name": "Pou Soldat",
+        "hp": 30,
+        "atk": 10,
+        "skills": [TapeFort, DanseLame, SlapThatAss, Jackpot]
+    },
+    "Pou Sergent": {
+        "name": "Pou Sergent",
+        "hp": 50,
+        "atk": 7,
+        "skills": [TapeFort, DanseLame, SlapThatAss, Heal]
+    },
+    "Pou Kamikaze": {
+        "name": "Pou Kamikaze",
+        "hp": 20,
+        "atk": 15,
+        "skills": [TapeFort, DanseLame, SlapThatAss, Heal]
+    }
+}
+
+""",
+    "Pou Accro Au Casino": {
+        "name": "Pou Kamikaze",
+        "hp": 20,
+        "atk": 15,
+        "skills": [Jab, BanditManchot, Jackpot, Heal]
+    },
+    "Pou BDSM": {
+        "name": "Pou Kamikaze",
+        "hp": 20,
+        "atk": 15,
+        "skills": [Jab, SlapThatAss, CireChaude, TapeFort]
+    }"""
