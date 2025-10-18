8d5bfadb190964d903a5d373deb7f4a77d5a52a1
LaMenace <butetjason26@gmail.com>
2025-10-18 02:48:52 +0200

    + make_team, create_team3

--- systems/team_creation.py
+++ systems/team_creation.py
@@ -23,11 +23,9 @@ TAUX_DROP={
     'légendaire':0.05
 }
 
-def create_team(owner_name): #lenT à rajouter -> 1 pour mes tests de comp plus rapide
+def make_team(basic_list, choice_list, x, choose_list): #lenT à rajouter -> 1 pour mes tests de comp plus rapide
     #Si tu veux implémenter le taux de drop sur la création de team juste créer une list = random_team(choice_list, TAUX_DROP, et len que tu veux)
     lenT = 3
-    basic_list = [Pou.from_model(owner_name, model) for model in PouModels.values()]
-    choice_list = random_team(basic_list, TAUX_DROP, 5)
     x = str(input("Voulez vous créer votre propre équipe ou en générer une aléatoirement ?\n\n     1 : Créez votre propre équipe !\n\n     2 : Générer une équipe (En dev)\n\n"))
     while (x != '1') and (x != '2'):
         x = str(input("Veuillez choisir une valeur valide : 1 ou 2.\n"))
@@ -35,22 +33,19 @@ def create_team(owner_name): #lenT à rajouter -> 1 pour mes tests de comp plus 
         pou_list = random_team(basic_list, TAUX_DROP, lenT)
         show_more(pou_list,1)
     else:
-        while True:
-            pou_list = []
-            # Avec enumerate on peut parcourir une liste et récupérer facielemnt à la fois index et valeur
-            for i, pou in enumerate(choice_list):
-                print(f"{i+1} : {pou.name} | {RED}Hp : {pou.hp}{RESET} | {BLUE}Atk : {pou.atk}{RESET}")
-            while len(pou_list) < lenT:
-                c = int(input('Choississez le pou que vous voulez : \n'))
-                while c not in (1,2,3,4,5):
-                    c = int(input('Veuillez choisir une valeur valide, un chiffre de 1 à 5 : \n'))
-                pou_list.append(choice_list[c-1])
-            show_more(pou_list,2)
-            #si 'o' on retourne dans le 'while true' et ça vide la pou_list, si 'n' on break et on renvoie la team comme la fonction se doit de le faire
-            confirm = str(input("Voulez vous modifier votre équipe ? (o/n)"))
-            if confirm == 'n':
-                break
-    return Team(owner_name,pou_list)
+        pou_list = []
+        # Avec enumerate on peut parcourir une liste et récupérer facielemnt à la fois index et valeur
+        '''for i, pou in enumerate(choice_list):
+            print(f"{i+1} : {pou.name} | {RED}Hp : {pou.hp}{RESET} | {BLUE}Atk : {pou.atk}{RESET}")'''
+        for z in choose_list:
+            pou_list.append(choice_list[z-1])
+        '''while len(pou_list) < lenT:
+            c = int(input('Choississez le pou que vous voulez : \n'))
+            while c not in (1,2,3,4,5):
+                c = int(input('Veuillez choisir une valeur valide, un chiffre de 1 à 5 : \n'))
+            pou_list.append(choice_list[c-1])'''
+        show_more(pou_list,2)
+    return pou_list
 
 
 #show_more et recup_flag purement optionnel mais rajoute un petit truc quand tu random_team
@@ -97,7 +92,63 @@ def random_team(pou_list, TAUX_D, lenT):#lenT -> variable pour len Team
             continue
     return random_list
     
+def recall_make_team(x):
+    if x == 'n':
+        return {'next_step' : 'NO_RECALL'}
+    else:
+        return {'next_step' : 'RECALL'}
+    
+'''def check_sub(str_numb):
+    str_stack = []
+    str_sub = []
+    str_res = ''
+    for i in str_numb:
+        if i in str_stack:
+            str_sub.append(i)
+        else:
+            str_stack.append(i)  Mdr je la laisse pour l'effort jme suis rendu compte que je pouvais faire plus simple je suis deg
+    str_res = ''.join(str_stack)
+    if len(str_sub)==0:
+        return str_res
+    else:
+        str_sub = ', '.join(str_sub)
+        return str_res, str_sub'''
 
+
+def create_team(owner_name):
+    basic_list = [Pou.from_model(owner_name, model) for model in PouModels.values()]
+    choice_list = random_team(basic_list, TAUX_DROP, 5)
+    poupou_list = []
+    choose_number = []
+    Taille_Que_Tu_Souhaites_My_Love_T_As_Vu_Pycharm_Je_Respecte_Les_Conventions_Mais_La_J_Imagine_Que_Tu_Vas_Dire_Que_Le_Nom_Est_Beaucoup_Beaucoup_Trop_Long_Et_Si_C_Est_Le_Cas_Je_Comprends = 3
+    while True:
+        display_manager('display_input', cas=1)
+        choice = str(input())
+        while choice not in ('1', '2'):
+            display_manager('invadlid')
+        break
+    match choice:
+        case '1':
+            while True:
+                display_manager('show_more', pou_list = choice_list)
+                while len(choose_number)<Taille_Que_Tu_Souhaites_My_Love_T_As_Vu_Pycharm_Je_Respecte_Les_Conventions_Mais_La_J_Imagine_Que_Tu_Vas_Dire_Que_Le_Nom_Est_Beaucoup_Beaucoup_Trop_Long_Et_Si_C_Est_Le_Cas_Je_Comprends:
+                    print("Quel pou choisissez vous ?\n")
+                    x = int(input())
+                    if x not in choose_number:
+                        choose_number.append(x)   #rajouter des prints
+                    else:
+                        display_manager('invalid')
+                        continue
+                poupou_list = make_team(basic_list, choice_list, choice, choose_number)
+                print("Voulez vous modifier votre team ?\n")
+                confirm = str(input())
+                step = recall_make_team(confirm)
+                if step['next_step'] == ['NO_RECALL']:
+                    break
+        case '2':
+            display_manager('show_more', pou_list=choice_list, cas=1)
+            return Team(owner_name,choice_list)
+    return Team(owner_name, poupou_list)
 
 
 
